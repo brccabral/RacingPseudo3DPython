eaa17f320610f8b17f98ac783ba40075fb97e182
Bruno Cabral <cabral.brc@gmail.com>
2022-04-20 10:52:02 -0700

    draw quad on screen

--- main.py
+++ main.py
@@ -2,11 +2,29 @@ import time
 import pygame
 import sys
 
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 768
+
+
+def drawQuad(
+    surface: pygame.Surface,
+    color: pygame.Color,
+    x1: int,
+    y1: int,
+    w1: int,
+    x2: int,
+    y2: int,
+    w2: int,
+):
+    pygame.draw.polygon(
+        surface, color, [(x1 - w1, y1), (x2 - w2, y2), (x2 + w2, y2), (x1 + w1, y1)]
+    )
+
 
 class GameWindow:
     def __init__(self):
         pygame.init()
-        self.window_surface = pygame.display.set_mode((640, 480))
+        self.window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         self.last_time = time.time()
         self.dt = 0
@@ -21,6 +39,8 @@ class GameWindow:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+            drawQuad(self.window_surface, "green", 500, 500, 200, 500, 300, 100)
 
             pygame.display.flip()
 
