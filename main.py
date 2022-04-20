57c2b2e775bb0c44773e92eb8a8e13b9d31973df
Bruno Cabral <cabral.brc@gmail.com>
2022-04-20 11:01:17 -0700

    class Line init

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import sys
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
 
+roadW = 2000  # road width (left to right)
+segL = 200  # segment length (top to bottom)
+camD = 0.84  # camera depth
+
+
+class Line:
+    def __init__(self):
+        self.x = self.y = self.z = 0.0  # game position (3D space)
+        self.X = self.Y = self.W = 0.0  # game position (2D projection)
+        self.scale = 0.0  # scale from camera position
+
+    def project(self, camX: int, camY: int, camZ: int):
+        self.scale = camD / (self.z - camZ)
+        self.X = (1 + self.scale * (self.x - self.camX)) * WINDOW_WIDTH / 2
+        self.Y = (1 - self.scale * (self.y - self.camY)) * WINDOW_HEIGHT / 2
+        self.W = self.scale * roadW * WINDOW_WIDTH / 2
+
 
 def drawQuad(
     surface: pygame.Surface,
@@ -24,6 +41,7 @@ def drawQuad(
 class GameWindow:
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption("Racing Pseudo 3D")
         self.window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         self.last_time = time.time()
